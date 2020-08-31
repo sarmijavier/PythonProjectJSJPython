bdbda20a96721167849594947b6c657edc09f632
jsbd10 <jsbenavides69@ucatolica.edu.co>
2020-08-31 17:59:10 -0500

    header, base.html and caroulsel

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from flask_bootstrap import Bootstrap
 
+app = Flask(__name__, template_folder='templates', static_folder='static')
 app = Flask(__name__)
+bootstrap = Bootstrap(app) 
 
 
 @app.route('/')
 def home():
-    return "<h1>Hola Perras!</h1>"
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
